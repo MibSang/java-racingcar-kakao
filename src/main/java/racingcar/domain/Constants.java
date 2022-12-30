0e9a378785406a3f33fbab294e271ebe05fc1a42
_Min <secured8372@gmail.com>
2022-12-30 11:47:55 +0900

    refactor: 상수 클래스 삭제
    상수 클래스 삭제
     - RandomStratedy로 관련 상수 이동

--- src/main/java/racingcar/domain/Constants.java
+++
@@ -1,8 +0,0 @@
-package racingcar.domain;
-
-public class Constants {
-    private Constants() {}
-
-    public static final int FORWARD_NUM = 4;
-    public static final int RANDOM_MAX_NUM = 10;
-}
--- src/main/java/racingcar/domain/stratedy/RandomStratedy.java
+++ src/main/java/racingcar/domain/stratedy/RandomStratedy.java
@@ -2,9 +2,10 @@ package racingcar.domain.stratedy;
 
 import java.util.Random;
 
-import static racingcar.domain.Constants.*;
-
 public class RandomStratedy implements MovableStrategy {
+    public static final int FORWARD_NUM = 4;
+    public static final int RANDOM_MAX_NUM = 10;
+
     public boolean isMove() {
         return new Random().nextInt(RANDOM_MAX_NUM) >= FORWARD_NUM;
     }
